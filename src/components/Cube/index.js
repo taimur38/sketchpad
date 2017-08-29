168797356b56a0fa82fe5f0c8513e98925a2379d
Taimur Shah <taimur38@gmail.com>
2017-08-28 20:55:11 -0400

    cube example

--- src/components/Cube/index.js
+++ src/components/Cube/index.js
@@ -7,7 +7,40 @@ export default class Cube extends Component {
 		super(props);
 	}
 
+	componentDidMount() {
+
+		this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+		this.camera.position.z = 400;
+
+		this.scene = new THREE.Scene();
+
+		const geometry = new THREE.BoxBufferGeometry(200, 200, 200);
+		const material = new THREE.MeshBasicMaterial({
+			color: Math.random() * 0xffffff 
+		});
+
+		this.mesh = new THREE.Mesh( geometry, material );
+		this.scene.add(this.mesh);
+
+		this.renderer = new THREE.WebGLRenderer();
+		this.renderer.setPixelRatio( window.devicePixelRatio );
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+
+		document.querySelector("#container").appendChild(this.renderer.domElement);
+		this.animate();
+	}
+
+	animate = () => {
+		requestAnimationFrame(this.animate);
+
+		this.mesh.rotation.x += 0.005;
+		this.mesh.rotation.y += 0.01;
+
+		this.renderer.render(this.scene, this.camera);
+	}
+
 	render() {
-		return <div>hello</div>
+		return <div id="container" />
 	}
 }
