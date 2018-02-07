914179c92a63862e6cd763f61b1cc8647d6cf35d
Taimur Shah <taimur38@gmail.com>
2018-02-07 17:41:11 -0500

    routes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -8,13 +8,10 @@ class App extends Component {
 			<div className="App">
 				<Link to="/metaball">Metaball</Link>
 				<Link to="/waves">Waves</Link>
-<<<<<<< HEAD
 				<Link to="/plane">Plane</Link>
-=======
 				<Link to="/sphere">Sphere</Link>
 				<Link to="/material">Material</Link>
 				<Link to="/cube">Cube</Link>
->>>>>>> 370c0413ff0ff99d2bfbb73b144d553cb4351acb
 			</div>
 		);
 	}
